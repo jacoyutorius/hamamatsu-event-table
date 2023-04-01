6878951869e3ee4c618a86907741dace5c8e1cd8
jacoyutorius <jacoyutorius@gmail.com>
2023-04-02 07:14:27 +0900

    AppSyncのAPIを追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import "@aws-amplify/ui-react/styles.css";
 import {
@@ -9,18 +9,48 @@ import {
   View,
   Card,
 } from "@aws-amplify/ui-react";
+import { Amplify, API } from 'aws-amplify';
+import * as queries from './graphql/queries';
+import { GraphQLQuery } from '@aws-amplify/api';
+import { AllQuery, HamamatsuEvents } from './API';
 
-import { Amplify } from 'aws-amplify';
 import config from './aws-exports';
 Amplify.configure(config);
 
-
 function App({ signOut }: any) {
+  const [records, setRecords] = useState<HamamatsuEvents[]>([])
+
+  useEffect(() => {
+    fetchRecords()
+  }, [])
+
+  async function fetchRecords() {
+    const allRecords = await API.graphql<GraphQLQuery<AllQuery>>(
+      { query: queries.all }
+    );
+    console.log(allRecords.data?.all.items);
+
+    if (allRecords.data) {
+      setRecords(allRecords.data.all.items);
+    }
+  }
+
   return (
     <View className="App">
       <Card>
         <Heading level={1}>We now have Auth!</Heading>
       </Card>
+
+      {
+        records.map(record => {
+          return (
+            <div>
+              { record.Description }
+            </div>
+          )
+        })
+      }
+
       <Button onClick={signOut}>Sign Out</Button>
     </View>
   );
---
+++ src/graphql/queries.ts
@@ -0,0 +1,151 @@
+/* tslint:disable */
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const all = /* GraphQL */ `
+  query All($limit: Int, $nextToken: String) {
+    all(limit: $limit, nextToken: $nextToken) {
+      items {
+        Key
+        Code
+        No
+        Prefecture
+        City
+        EventName
+        EventNameKana
+        EventNameEn
+        StartedOn
+        EndedOn
+        StartedAt
+        EndedAt
+        StartedAtNote
+        Description
+        Price
+        PriceDetail
+        Contact
+        Tel
+        TelExtension
+        Organizer
+        Place
+        PlaceAddress
+        Formula
+        Latitude
+        Longitude
+        Access
+        Parking
+        Capacity
+        ClosingOn
+        ClosingAt
+        HowToJoin
+        Url
+        Note
+        Category
+        Distinct
+        OpenedAt
+        UpdatedAt
+        ChildInformation
+        FacilityNo
+        ImportedAt
+        StartMonth
+      }
+      nextToken
+    }
+  }
+`;
+export const queryByMonth = /* GraphQL */ `
+  query QueryByMonth($month: String!) {
+    queryByMonth(month: $month) {
+      items {
+        Key
+        Code
+        No
+        Prefecture
+        City
+        EventName
+        EventNameKana
+        EventNameEn
+        StartedOn
+        EndedOn
+        StartedAt
+        EndedAt
+        StartedAtNote
+        Description
+        Price
+        PriceDetail
+        Contact
+        Tel
+        TelExtension
+        Organizer
+        Place
+        PlaceAddress
+        Formula
+        Latitude
+        Longitude
+        Access
+        Parking
+        Capacity
+        ClosingOn
+        ClosingAt
+        HowToJoin
+        Url
+        Note
+        Category
+        Distinct
+        OpenedAt
+        UpdatedAt
+        ChildInformation
+        FacilityNo
+        ImportedAt
+        StartMonth
+      }
+      nextToken
+    }
+  }
+`;
+export const getOne = /* GraphQL */ `
+  query GetOne($Key: ID!) {
+    getOne(Key: $Key) {
+      Key
+      Code
+      No
+      Prefecture
+      City
+      EventName
+      EventNameKana
+      EventNameEn
+      StartedOn
+      EndedOn
+      StartedAt
+      EndedAt
+      StartedAtNote
+      Description
+      Price
+      PriceDetail
+      Contact
+      Tel
+      TelExtension
+      Organizer
+      Place
+      PlaceAddress
+      Formula
+      Latitude
+      Longitude
+      Access
+      Parking
+      Capacity
+      ClosingOn
+      ClosingAt
+      HowToJoin
+      Url
+      Note
+      Category
+      Distinct
+      OpenedAt
+      UpdatedAt
+      ChildInformation
+      FacilityNo
+      ImportedAt
+      StartMonth
+    }
+  }
+`;
