4733205b020b56d40dc21b18ad937734ea0a0176
jacoyutorius <jacoyutorius@gmail.com>
2023-04-14 04:21:12 +0900

    カレンダー形式で表示するように変更

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,15 @@ import './App.css';
 import "@aws-amplify/ui-react/styles.css";
 import { withAuthenticator } from "@aws-amplify/ui-react";
 import { Amplify } from 'aws-amplify';
-import { Pagination, Toast } from "flowbite-react"
+import dayjs from 'dayjs';
 
 // components
+import { Toast } from "flowbite-react"
 import { NavbarComponent } from './components/Navbar';
 import { ModalComponent } from './components/Modal';
 import { CreateFormComponent } from "./components/CreateForm"
-import { CardListComponent } from './components/CardList';
+// import { CardListComponent } from './components/CardList';
+import { CalendarComponent } from './components/Calendar';
 import { FooterComponent } from './components/Footer';
 
 // contexts
@@ -48,11 +50,26 @@ const NoCard = () => {
   );
 }
 
+function getMonth(year:number, month:number) {
+  const firstDayOfTheMonth = dayjs(new Date(year, month, 1)).day()
+  let currentMonthCount = 0 - firstDayOfTheMonth
+  const daysMatrix = new Array(6).fill([]).map(() => {
+    return new Array(7).fill(null).map(() => {
+      currentMonthCount++
+      return dayjs(new Date(year, month, currentMonthCount))
+    })
+  })
+
+  return daysMatrix
+}
+
 function App({ signOut }: any) {
   const [createModalOpen, setCreateModalOpen] = useState(false)
   const toggleCreateModal = () => { setCreateModalOpen(!createModalOpen) }
   const [month, setMonth] = useState("202304")
   const [eventList, setEventList] = useState<any>([])
+
+  const calendar = getMonth(Number(month.substring(0, 4)), Number(month.substring(4, 6)) - 1)
 
   return (
     <EventListContext.Provider value={ {eventList, setEventList} }>
@@ -68,7 +85,11 @@ function App({ signOut }: any) {
           {/* ナビバー */}
           <NavbarComponent onClick={toggleCreateModal} signOut={signOut} />
 
-          { eventList.length === 0 &&
+          <div className="h-screen flex flex-col mb-20 md:mx-2">
+            <CalendarComponent calendar={calendar}></CalendarComponent>
+          </div>
+
+          {/* { eventList.length === 0 &&
             <div className="flex justify-center my-32">
               <NoCard /> 
             </div>}
@@ -77,7 +98,7 @@ function App({ signOut }: any) {
             // NOTE: 一番最後のカードがフッタに隠れてしまうため、mbを指定している
              <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 mb-20 lg:mx-28">
               <CardListComponent />
-            </div>}
+            </div>} */}
           
           <FooterComponent />
         </div>
---
+++ src/components/Calendar/CalendarDayComponent.tsx
@@ -0,0 +1,87 @@
+export type CalendarDayEventProps = {
+  eventName: string,
+  url: string | null,
+  category: string
+}
+
+export type CalendarDayComponentProps = {
+  day: any,
+  isFirstWeek: boolean,
+  events: Array<CalendarDayEventProps>
+}
+
+type LinkProps = {
+  text: string,
+  url: string,
+  category: string
+}
+
+const Link = ({ text, url, category }: LinkProps): JSX.Element => {
+  return (
+    <a href={url} className={ `${getLabelColor(category)} text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300` } target="_blank" rel="noreferrer">
+      {text}
+    </a>
+  )
+}
+
+function getLabelColor(category: string) {
+  if (category === "イベント") {
+    return "bg-blue-50 text-blue-600"
+  }
+  else if (category === "おしらせ") {
+    return "bg-orange-50 text-orange-600"
+  }
+  else if (category === "こそだて") {
+    return "bg-green-50 text-green-600"
+  }
+  else if (category === "けんこう") { 
+    return "bg-amber-50 text-amber-600"
+  }
+  else if (category === "そうだん") {
+    return "bg-rose-50 text-rose-600"
+  }
+  else if (category === "おんがく") {
+    return "bg-cyan-50 text-cyan-600"
+  }
+  else if (category === "講座・教室") {
+    return "bg-lime-50 text-lime-600"
+  }
+  else {
+    console.log({category})
+    return "bg-gray-50 text-gray-800"
+  }
+}
+
+function getDateColor(day:any) {
+  const date = day.format("d")
+  
+  if (date === '0') {
+    return "bg-red-100"
+  }
+  else if (date === '6') {
+    return "bg-blue-100"
+  }
+  else {
+    return "bg-gray-100"
+  }
+}
+
+export const CalendarDayComponent = ({ day, isFirstWeek, events }: CalendarDayComponentProps): JSX.Element => {
+  const linkComponents = events.map((event:CalendarDayEventProps) => {
+    return (<div>
+      <Link text={ event.eventName } url={ event.url || "" } category={ event.category }></Link>
+    </div>)
+  })
+
+  const headBgColor = getDateColor(day)
+
+  return (<div className="border border-gray-200 flex flex-col">
+    <header className={`flex flex-col items-center ${headBgColor}`}>
+      {isFirstWeek && <p className="text-sm mt-1">{day.format("ddd")}</p>}
+        <p className={"text-md p-1 my-1 text-center"}>{day.format("DD")}</p>
+    </header>
+    <div className="p-2">
+      { linkComponents }
+    </div>
+  </div>)
+}
