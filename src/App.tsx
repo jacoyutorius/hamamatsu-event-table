50720ceb4a718083b61ae516490d844423289031
jacoyutorius <jacoyutorius@gmail.com>
2023-04-18 06:01:22 +0900

    CloudWatchRUM を設定

--- src/App.tsx
+++ src/App.tsx
@@ -21,6 +21,34 @@ import { EventListContext } from './hooks/EventListContext';
 // amplify config
 import config from './aws-exports';
 
+// clouswatch RUM
+import { AwsRum, AwsRumConfig } from 'aws-rum-web';
+
+try {
+  const config: AwsRumConfig = {
+    sessionSampleRate: 1,
+    guestRoleArn: "arn:aws:iam::865422985541:role/RUM-Monitor-ap-northeast-1-865422985541-7532315671861-Unauth",
+    identityPoolId: "ap-northeast-1:e2014572-68e5-48d5-be28-db1d5dc571e3",
+    endpoint: "https://dataplane.rum.ap-northeast-1.amazonaws.com",
+    telemetries: ["performance","errors","http"],
+    allowCookies: true,
+    enableXRay: false
+  };
+
+  const APPLICATION_ID: string = '8d9cac35-e51a-4bef-9f10-47f40a637565';
+  const APPLICATION_VERSION: string = '1.0.0';
+  const APPLICATION_REGION: string = 'ap-northeast-1';
+
+  const awsRum: AwsRum = new AwsRum(
+    APPLICATION_ID,
+    APPLICATION_VERSION,
+    APPLICATION_REGION,
+    config
+  );
+} catch (error) {
+  // Ignore errors thrown during CloudWatch RUM web client initialization
+}
+
 // NOTE: AmplifyにデプロイするとAppSync関連の設定情報が生成されない様子なので
 //  環境変数から読み込んでAmplify.configureに渡すようにしている。
 const GraphQlConfig: Object = {
