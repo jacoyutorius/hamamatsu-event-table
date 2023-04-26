122d15faf0d2ce7a2d7facec21b075317dfbb7eb
jacoyutorius <jacoyutorius@gmail.com>
2023-04-27 05:15:42 +0900

    最大取得件数がうまく設定できなかったのでもとに戻す

--- src/hooks/useEventList.ts
+++ src/hooks/useEventList.ts
@@ -8,18 +8,18 @@ import { QueryByMonthQuery } from '../API';
 import { SelectedMonthContext } from './SelectedMonthContext'
 import { EventListContext } from './EventListContext'
 
-// NOTE: とりあえず最大200レコード取得するようにしている。
-// TODO: ほんとうはページネーションとかしたほうがいいのだが。
-const LIMIT = 200
-
 export const useEventList = () => {
   const { month, setMonth } = useContext(SelectedMonthContext)
   const { setEventList } = useContext(EventListContext)
 
+  // NOTE: とりあえず最大200レコード取得するようにしている。
+  // TODO: ほんとうはページネーションとかしたほうがいいのだが。
+  const limit = 200
+
   useEffect(() => {
     const fetch = async () => {
       const records = await API.graphql<GraphQLQuery<QueryByMonthQuery>>(
-        graphqlOperation(queries.queryByMonth, { month, LIMIT })
+        graphqlOperation(queries.queryByMonth, { month, limit })
       );
       const list = records.data?.queryByMonth.items;
       console.log(month, `${list?.length} events`)
